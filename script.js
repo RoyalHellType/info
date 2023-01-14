3fe8d6da572652915cac681714c9c20d3d662f6d
RoyalHell <69813442+RoyalHellType@users.noreply.github.com>
2023-01-15 00:53:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function waitAndResponce(inputText) {
     switch (inputText.toLowerCase().trim()) {
         case "intro":
             setTimeout(() => {
-                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Vinayak Ramchandra Patil</a>.</span><br><br>I am a Computer Engineering student at <span class='bold'>Ch. Shivaji University 👨🏻‍💻📚</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+                sendTextMessage("Здравствуйте товарищи,<br><br>Моё имя <span class='bold'><a class='alink'>Королевский Ад</a>.</span><br><br>Я частенько провожу трансляции, тоесть <span class='bold'>стример... 🫥</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
             }, 2000);
             break;
         case "help":
